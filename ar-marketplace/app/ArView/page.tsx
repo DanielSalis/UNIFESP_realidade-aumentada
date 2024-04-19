ac5c7248f248cc5fc24101ba81180c82fd86c611
DanielSalis <danielsalis98@gmail.com>
2024-04-19 11:55:34 -0300

    feat: run AR view into next app

---
+++ ar-marketplace/app/ArView/page.tsx
@@ -0,0 +1,210 @@
+/* eslint no-use-before-define: 0 */  // -->
+//@ts-nocheck
+'use client';
+import "./App.css";
+import * as THREE from "three";
+import { ARButton } from "three/examples/jsm/webxr/ARButton";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { XREstimatedLight } from "three/examples/jsm/webxr/XREstimatedLight";
+import { useEffect } from "react";
+
+function ArScene() {
+  let reticle;
+  let hitTestSource = null;
+  let hitTestSourceRequested = false;
+
+  let scene, camera, renderer;
+
+  let models = ["./dylan_armchair_yolk_yellow.glb", "./sofa2.glb"];
+  let modelScaleFactor = [0.01, 0.03];
+  let items = [];
+  let itemSelectedIndex = 0;
+
+  let controller;
+
+  useEffect(() => {
+    init();
+    setupFurnitureSelection();
+    animate();
+  }, []);
+
+  const init = () => {
+    let myCanvas = document.getElementById("canvas");
+    if (myCanvas == null) return;
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+      70,
+      myCanvas.innerWidth / myCanvas.innerHeight,
+      0.01,
+      20
+    );
+
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    light.position.set(0.5, 1, 0.25);
+    scene.add(light);
+
+    renderer = new THREE.WebGLRenderer({
+      canvas: myCanvas,
+      antialias: true,
+      alpha: true,
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(myCanvas.innerWidth, myCanvas.innerHeight);
+    renderer.xr.enabled = true;
+
+    const xrLight = new XREstimatedLight(renderer);
+    xrLight.addEventListener("estimationstart", () => {
+      scene.add(xrLight);
+      scene.remove(light);
+      if (xrLight.environment) {
+        scene.environment = xrLight.environment;
+      }
+    });
+
+    xrLight.addEventListener("estimationend", () => {
+      scene.add(light);
+      scene.remove(xrLight);
+    });
+
+    let arButton = ARButton.createButton(renderer, {
+      requiredFeatures: ["hit-test"],
+      optionalFeatures: ["dom-overlay", "light-estimation"],
+      domOverlay: { root: document.body },
+    });
+    arButton.style.bottom = "20%";
+    console.log(arButton)
+    document.body.appendChild(arButton);
+
+    for (let i = 0; i < models.length; i++) {
+      const loader = new GLTFLoader();
+      loader.load(models[i], function (glb) {
+        let model = glb.scene;
+        items[i] = model;
+      });
+    }
+
+    controller = renderer.xr.getController(0);
+    controller.addEventListener("select", onSelect);
+    scene.add(controller);
+
+    reticle = new THREE.Mesh(
+      new THREE.RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2),
+      new THREE.MeshBasicMaterial()
+    );
+    reticle.matrixAutoUpdate = false;
+    reticle.visible = false;
+    scene.add(reticle);
+  };
+
+  const onSelect = () => {
+    if (reticle.visible) {
+      let newModel = items[itemSelectedIndex].clone();
+      newModel.visible = true;
+      reticle.matrix.decompose(
+        newModel.position,
+        newModel.quaternion,
+        newModel.scale
+      );
+      let scaleFactor = modelScaleFactor[itemSelectedIndex];
+      newModel.scale.set(scaleFactor, scaleFactor, scaleFactor);
+
+      scene.add(newModel);
+    }
+  };
+
+  const onClicked = (e, selectItem, index) => {
+    itemSelectedIndex = index;
+    for (let i = 0; i < models.length; i++) {
+      const el = document.querySelector(`#item` + i);
+      el.classList.remove("clicked");
+    }
+    e.target.classList.add("clicked");
+  };
+
+  const setupFurnitureSelection = () => {
+    for (let i = 0; i < models.length; i++) {
+      const el = document.querySelector(`#item` + i);
+      if (el != null) {
+        el.addEventListener("beforexrselect", (e) => {
+          e.preventDefault();
+          e.stopPropagation();
+        });
+        el.addEventListener("click", (e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          onClicked(e, items[i], i);
+        });
+      }
+    }
+  };
+
+  const animate = () => {
+    if (renderer !== null && renderer !== undefined) {
+      renderer.setAnimationLoop(render);
+    }
+  };
+
+  const render = (timestamp, frame) => {
+    if (frame) {
+      const elements = document.querySelectorAll('#ARButton');
+      debugger
+        elements.forEach(element => {
+          element.style.display = 'none';
+      });
+      const referenceSpace = renderer.xr.getReferenceSpace();
+      const session = renderer.xr.getSession();
+
+      if (hitTestSourceRequested === false) {
+        session.requestReferenceSpace("viewer").then(function (referenceSpace) {
+          session
+            .requestHitTestSource({ space: referenceSpace })
+            .then(function (source) {
+              hitTestSource = source;
+            });
+        });
+
+        session.addEventListener("end", function () {
+          hitTestSourceRequested = false;
+          hitTestSource = null;
+        });
+
+        hitTestSourceRequested = true;
+      }
+
+      if (hitTestSource) {
+        const hitTestResults = frame.getHitTestResults(hitTestSource);
+
+        if (hitTestResults.length) {
+          const hit = hitTestResults[0];
+
+          reticle.visible = true;
+          reticle.matrix.fromArray(
+            hit.getPose(referenceSpace).transform.matrix
+          );
+        } else {
+          reticle.visible = false;
+        }
+      }
+    }
+
+    renderer.render(scene, camera);
+  };
+
+  return (
+    <div className="ar-scene">
+      <canvas id="canvas"></canvas>
+      <div className="navbar">
+        <button onClick={()=>window.location.reload()} style={{margin: "0 10px"}}> Voltar</button >
+        <img
+          alt="armchair"
+          className="button-image"
+          id="item0"
+          src="/armchair.png"
+        />
+        <img alt="sofa" className="button-image" id="item1" src="/sofa2.png" />
+      </div>
+    </div>
+  );
+}
+
+export default ArScene;
